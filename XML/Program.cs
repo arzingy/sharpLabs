84a8d48095205613ee84e4b78c74de9a9e90c48a
Arseniy <79666503+arzingy@users.noreply.github.com>
2021-09-29 12:08:11 +0300

    Add files to XML lab

---
+++ XML/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xd = new XmlDocument();
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            settings.ValidationType = ValidationType.DTD;
+            using (XmlReader reader = XmlReader.Create("Lab_work.xml",
+            settings))
+            {
+                try
+                {
+                    xd.Load(reader);
+                }
+                catch (XmlSchemaException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+            using (XmlReader reader = new XmlTextReader("Lab_work.xml"))
+            {
+                bool next_line = true;
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            for (int i = 0; i < reader.AttributeCount; i++)
+                            {
+                                reader.MoveToNextAttribute();
+                                Console.WriteLine(@" {0} = {1}", reader.Name, reader.Value);
+                            }
+                            reader.MoveToElement();
+           
+                            if (next_line)
+                            {
+                                Console.WriteLine("///////////////////////////////////");
+                                next_line = false;
+                            }
+                            else next_line = true;
+
+                            break;
+
+                    }
+                }
+            }
+
+        }
+    }
+}
